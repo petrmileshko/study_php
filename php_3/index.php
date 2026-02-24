5f657ca0349ecbe47514410d04dc3ae2a01be2e7
Peter <mileshko.petr@yandex.ru>
2026-02-24 21:05:41 +0300

    Запуск локального сервера и вывод в браузер
    добавлена задача php 3

---
+++ php_3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 3</title>
+</head>
+<body>
+    <?php
+    echo "Привет мир!";
+    ?>
+</body>
+</html>
