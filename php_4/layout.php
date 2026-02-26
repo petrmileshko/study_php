c0050895887b1b3f5e8c573f51d0b11a21e27424
Peter <mileshko.petr@yandex.ru>
2026-02-26 19:06:15 +0300

    Подключение файлов

---
+++ php_4/layout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 4</title>
+</head>
+<body>
+    <h1>Задание №4</h1>
+    <p>
+        <?php ?>
+    </p>
+    <p>
+<!-- Это комментарий  не будет виден в браузере -->
+    </p>
+</body>
+</html>
